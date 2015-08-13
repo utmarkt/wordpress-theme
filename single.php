ec53484d67e6250483c800d03fbf35b4faf629a3
Jeanne Schiller <schiller.jeanne@gmail.com>
2015-08-12 23:44:31 -0400

    single page styles

--- single.php
+++ single.php
@@ -1,46 +1,53 @@
-<?php get_header(); ?>
+<div class="single-container">
+  <?php get_header(); ?>
 
-<div class="main">
-  <div class="container">
+  <div class="main">
+    <div class="container">
 
-    <div class="content">
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+      <div class="content">
+        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
 
-          <div class="entry-meta">
-            <?php hackeryou_posted_on(); ?>
-          </div><!-- .entry-meta -->
+            <div class="entry-meta">
+              <?php hackeryou_posted_on(); ?>
+            </div><!-- .entry-meta -->
 
-          <div class="entry-content">
-            <?php the_content(); ?>
-            <?php wp_link_pages(array(
-              'before' => '<div class="page-link"> Pages: ',
-              'after' => '</div>'
-            )); ?>
-          </div><!-- .entry-content -->
+            <div class="entry-content">
+              <?php 
+                  if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                  the_post_thumbnail(large);
+                } 
+              ?>
+              <?php the_content(); ?>
+              <?php wp_link_pages(array(
+                'before' => '<div class="page-link"> Pages: ',
+                'after' => '</div>'
+              )); ?>
+            </div><!-- .entry-content -->
 
-          <div class="entry-utility">
-            <?php hackeryou_posted_in(); ?>
-            <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
-          </div><!-- .entry-utility -->
-        </div><!-- #post-## -->
+            <div class="entry-utility">
+              <?php hackeryou_posted_in(); ?>
+              <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+            </div><!-- .entry-utility -->
+          </div><!-- #post-## -->
 
-        <div id="nav-below" class="navigation">
-          <p class="nav-previous"><?php previous_post_link('%link', '&larr; %title'); ?></p>
-          <p class="nav-next"><?php next_post_link('%link', '%title &rarr;'); ?></p>
-        </div><!-- #nav-below -->
+          <div id="nav-below" class="navigation">
+            <p class="nav-previous"><?php previous_post_link('%link', '&larr; %title'); ?></p>
+            <p class="nav-next"><?php next_post_link('%link', '%title &rarr;'); ?></p>
+          </div><!-- #nav-below -->
 
-        <?php comments_template( '', true ); ?>
+          <?php comments_template( '', true ); ?>
 
-      <?php endwhile; // end of the loop. ?>
+        <?php endwhile; // end of the loop. ?>
 
-    </div> <!-- /.content -->
+      </div> <!-- /.content -->
 
-    <?php get_sidebar(); ?>
+      <?php get_sidebar(); ?>
 
-  </div> <!-- /.container -->
-</div> <!-- /.main -->
+    </div> <!-- /.container -->
+  </div> <!-- /.main -->
 
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
+</div> <!-- /.single-container -->
